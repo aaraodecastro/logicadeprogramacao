d5007d58649710fe09d1c11fe011e1b1c3be2cee
aaraodecastro <aaraodecastro@gmail.com>
2025-08-20 16:40:48 -0300

    Arrumei

--- aula1/atividade6.py
+++ aula1/atividade6.py
@@ -11,10 +11,9 @@ segundo_numero=int(input("Digite um número inteiro: "))
 
 pr=primeiro_numero-segundo_numero
 while pr!=0:
-    if primeiro_numero != segundo_numero:
-        if primeiro_numero>segundo_numero:
-            primeiro_numero=primeiro_numero-segundo_numero
-        else: segundo_numero=segundo_numero-primeiro_numero
+    if primeiro_numero>segundo_numero:
+        primeiro_numero=primeiro_numero-segundo_numero
+    else: segundo_numero=segundo_numero-primeiro_numero
     pr=primeiro_numero-segundo_numero
 
 print(primeiro_numero)
