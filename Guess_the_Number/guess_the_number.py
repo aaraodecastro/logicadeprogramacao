0af009d474c763e19c37a3befc8b2d653fe50bbd
aaraodecastro <aaraodecastro@gmail.com>
2025-08-20 18:22:02 -0300

    Arrumei os nomes

--- Guess_the_Number/guess_the_number.py
+++ Guess_the_Number/guess_the_number.py
@@ -3,14 +3,14 @@ import random
 try:
     
     numerocerto = random.randint(1,10)
-    numerousuario=int(input("Digite um número de 1 a 10"))
+    numerousuario=int(input("Digite um número de 1 a 10:"))
     t=1
     while numerousuario!=numerocerto:
         t=t+1
         if numerousuario>numerocerto:
-            numerousuario=int(input(f"Muito alto,tente um número menor que{numerousuario}:"))
+            numerousuario=int(input(f"Muito alto,tente um número menor que {numerousuario}:"))
         else:
-            numerousuario=int(input(f"Muito baixo,tente um número maior que:{numerousuario}:"))
+            numerousuario=int(input(f"Muito baixo,tente um número maior que {numerousuario}:"))
     print(f"Parabens, você acertou em {t} vezes.")
 except:
     print("Entrada não válida")
