34861a82d82d47258a4b81af3af799537a2136ff
aaraodecastro <aaraodecastro@gmail.com>
2025-08-20 17:31:14 -0300

    Concertei o codigo

--- Guess_the_Number/guess_the_number.py
+++ Guess_the_Number/guess_the_number.py
@@ -1,12 +1,12 @@
-nunerousuario=int(input("Digite um número de 1 a 10"))
+numerousuario=int(input("Digite um número de 1 a 10"))
 numerocerto=7
-t=0
-while nunerousuario!=numerocerto:
+t=1
+while numerousuario!=numerocerto:
     t=t+1
-    if nunerousuario>numerocerto:
-        nunerousuario=int(input(f"Muito alto,tente um número menor que:{n}"))
+    if numerousuario>numerocerto:
+        numerousuario=int(input(f"Muito alto,tente um número menor que{numerousuario}:"))
     else:
-        nunerousuario=int(input(f"Muito alto,tente um número maior que:{n}"))
+        numerousuario=int(input(f"Muito baixo,tente um número maior que:{numerousuario}:"))
 print(f"Parabens, você acertou em {t} vezes.")
 
     
