d4a4b077e5552d187f53c96edb625dcc67cadc47
aaraodecastro <aaraodecastro@gmail.com>
2025-09-27 22:05:17 -0300

    Update prova_1.py

--- Aulas/prova_1.py
+++ Aulas/prova_1.py
@@ -1,6 +1,6 @@
-tempo_servico = int(input("Digite o tempo de serviço em anos: ")) 
-valor_do_fgts = 10000
+tempo_servico = float(input("Digite o tempo de serviço em anos: ")) 
+valor_do_fgts = float(input("Digite o valor do FGTS: "))
 if tempo_servico > 1:
-    print(f"Você tem direito a multa do FGTS, o valor a receber é: {valor_do_fgts*0.4}.")
+    print(f"Você tem direito a multa do FGTS, o valor a receber é: {valor_do_fgts*1.4}.")
 else:
     print("Não haverá multa.")
